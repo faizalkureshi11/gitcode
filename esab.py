01e2fed99dfd147e20213a6dfca26ee87d7725fe
prinzefaizal <faizal.kureshi11@gmail.com>
2020-04-05 03:21:53 +0530

    Update esab.py

--- esab.py
+++ esab.py
@@ -3,28 +3,29 @@ from itertools import combinations
 import numpy as np
 import sys
 
-def cmp(bit): return [
+def cmp(bit):
+ return [
     '_' if bt == '_' else 
     '1' if bt =='0' else '0' 
     for bt in bit
 ]
 
 class qarr():
-    def __init__(self, BT):
-        self.bit = ['_'] * BT
-        self.unkown = list(range(BT))
+    def __init__(self, bt):
+        self.bit = ['_'] * bt
+        self.unkown = list(range(bt))
         self.read(10)
         self.run()
 
-    def read(self, nn):
-        for _ in range(nn):
-            i = self.unkown.pop()
-            self.bit[i] = self.readbits(i+1)
+    def read(self, no):
+        for _ in range(no):
+            k = self.unkown.pop()
+            self.bit[k] = self.readbits(k+1)
             self.unkown = self.unkown[::-1]
         self.updatestates()
 
-    def readbits(self, i):
-        print(i, flush=True)
+    def readbits(self, k):. #reading bits
+        print(k, flush=True)
         return input()
 
     def updatestates(self):
@@ -33,8 +34,8 @@ class qarr():
         self.bitsr  = cmp(self.bitscr)
         self.state = [self.bit, self.bitsc, self.bitscr, self.bitsr]
 
-    def get_test_index(self):
-        candid = list(set(range(BB)) - set(self.unkown))
+    def getestindex(self):
+        candid = list(set(range(bb)) - set(self.unkown))
         maxstates = len(set(map(tuple, self.state)))
         for index in combinations(candid, 2):
             numstates = len(set(
@@ -43,26 +44,26 @@ class qarr():
             ))
             if numstates == maxstates: return index
 
-    def colle(self):        
-        test_index = self.get_test_index()
-        test = [self.readbits(i+1) for i in test_index]
+    def colp(self):        #collapse function
+        test_index = self.getestindex()
+        test = [self.readbits(k+1) for k in test_index]
                 
         self.bit = next(state
             for state in self.state
             if test == list(np.take(state, test_index))
         )
 
-    def run(self):
+    def run(self):.  #function to run
         while True:
-            self.colle()
+            self.colp()
             try: self.read(8)
             except IndexError: break
     
 
-tt, BB = map(int, input().split())
-print('B:', BB, file=sys.stderr)
+tt, bb = map(int, input().split()) #input and join bit in array
+print('B:', bb, file=sys.stderr)
 for _ in range(tt): 
-    array = qarr(BB)
+    array = qarr(bb)
     print('guess:', ''.join(array.bit), file=sys.stderr)
     print(''.join(array.bit), flush=True)
     if input() == 'N': sys.exit() 
